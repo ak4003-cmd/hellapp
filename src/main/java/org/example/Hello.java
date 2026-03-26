e994152ad37705076b3330eab81620d8453c1e8a
ak4003-cmd <ak4003@srmist.edu.in>
2026-03-26 08:57:44 +0530

    Revert "UC3 completed"
    This reverts commit 48e9e258ddd624c972aef29d4a54e26f3b8f8c7d.

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -2,10 +2,7 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
-        }
+        String name = args[0];
         System.out.println("Hello, " + name + "!");
     }
 }
