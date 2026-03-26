188ceb077a0684722b802396d0aa075b7d3ed867
ak4003-cmd <ak4003@srmist.edu.in>
2026-03-26 08:49:11 +0530

    UC2 completed

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
